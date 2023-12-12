b686acb5909ac2263fee37e964103c1936e1dee1
imran <87297004+imranshaikh9930@users.noreply.github.com>
2023-12-12 13:05:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
 //complete this code
-class Animal {}
+class Animal {
 
-class Dog extends Animal {}
+	constructor(species){
+		this.species = species;
+	}
 
-class Cat extends Animal {}
+	get species(){
+		return this.species;
+	}
+
+	makeSound(){
+		console.log("animal sound")
+	}
+}
+
+class Dog extends Animal {
+	constructor(species) {
+		 super(species);
+  }
+
+	 bark() {
+    console.log("Woof");
+  }
+
+  
+  makeSound() {
+    console.log("Bark");
+  }
+}
+
+class Cat extends Animal {
+	constructor(species) {
+	   super(species);
+  }
+	 purr() {
+    console.log("Purr");
+  }
+
+  
+  makeSound() {
+    console.log("Meow");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
